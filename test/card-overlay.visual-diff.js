e24ffdee63139e55f4876866cdf2c4d107878da3
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-12-07 14:58:24 -0500

    feat: switching to ESM-by-default & upgrading actions

--- test/card-overlay.visual-diff.js
+++ test/card-overlay.visual-diff.js
@@ -1,9 +1,9 @@
-const puppeteer = require('puppeteer');
-const VisualDiff = require ('@brightspace-ui/visual-diff');
+import puppeteer from 'puppeteer';
+import { VisualDiff } from '@brightspace-ui/visual-diff';
 
 describe('d2l-labs-card-overlay', () => {
 
-	const visualDiff = new VisualDiff('card-overlay', __dirname);
+	const visualDiff = new VisualDiff('card-overlay', import.meta.url);
 
 	let browser, page;
 
